1c470008671154d8af2aa7af8f73487e5bcb2cd0
Nakamura <dacani97@gmail.com>
2024-01-30 23:31:00 -0500

    Fast Forward: means

--- means.py
+++ means.py
@@ -99,9 +99,9 @@ def read_excel_pedestrian(excel_path
     LOGGER.info(f"Lectura de excel:\n{excel_path}")
     ws = wb['Inicio']
 
-    num_giro_i = []
+    num_giro_i = [0,0,0,0]
 
-    for s in turn_slice:
+    for j, s in enumerate(turn_slice):
         aux = []
         for row in ws[s]:
             aux.append(row[0].value)
@@ -110,11 +110,11 @@ def read_excel_pedestrian(excel_path
             quant = aux.index(None)
         except ValueError: #TODO: Falta analizar el caso en que haya una letra allí!
             quant = len(aux)
-        num_giro_i.append(quant)
+        num_giro_i[j] = quant
+    
+    ws = wb['Data Peatonal']
 
     #Reading data from sheets
-    CAR_LIST = []
-    MOTO_LIST = []
     list_A = [[cell.value for cell in row] for row in ws[matrix_slice]]
 
     for i, row in enumerate(list_A):
@@ -125,40 +125,58 @@ def read_excel_pedestrian(excel_path
     A = np.array(list_A, dtype="float")
     A[np.isnan(A)] = 0
 
-    MORNING = []
-    EVENING = []
-    NIGHT   = []
-    for i_giro in range(4):
-        MORNING.append(
+    morning = []
+    evening = []
+    night   = []
+
+    INCREMENTOS = [0,140,280,420]
+
+    for incremento, giros in zip(INCREMENTOS, num_giro_i):
+        morning.append(
             np.array(
                 [
-                    A[2:13, (10*ped_type):(10*ped_type+num_giro_i[i_giro])] #El i_giro esta mal enfocado
-                    for ped_type in range(16)
+                    A[2:14, (10*ped_type+incremento):(10*ped_type+giros+incremento)]
+                    for ped_type in range(4) #Kid, Adult, Old, WC
                 ]
             )
         )
-        EVENING.append(
+        evening.append(
             np.array(
                 [
-                    A[24:35, (10*ped_type):(10*ped_type+num_giro_i[i_giro])]
-                    for ped_type in range(16)
+                    A[24:36, (10*ped_type+incremento):(10*ped_type+giros+incremento)]
+                    for ped_type in range(4) #Kid, Adult, Old, WC
                 ]
             )
         )
-        NIGHT.append(
+        night.append(
             np.array(
                 [
-                    A[46:57, (10*ped_type):(10*ped_type+num_giro_i[i_giro])]
-                    for ped_type in range(16)
+                    A[46:58, (10*ped_type+incremento):(10*ped_type+giros+incremento)]
+                    for ped_type in range(4) #Kid, Adult, Old, WC
                 ]
             )
         )
 
     wb.close()
 
+    MORNING = []
+    EVENING = []
+    NIGHT = []
+
+    for morning_i, evening_i, night_i in zip(morning, evening, night):
+        morning_concatenated = np.concatenate(morning_i, axis = 1)
+        evening_concatenated = np.concatenate(evening_i, axis = 1)
+        night_concatenated = np.concatenate(night_i, axis = 1)
+        MORNING.append(morning_concatenated)
+        EVENING.append(evening_concatenated)
+        NIGHT.append(night_concatenated)
+
+    MORNING = np.concatenate(MORNING, axis = 1)
+    EVENING = np.concatenate(EVENING, axis = 1)
+    NIGHT = np.concatenate(NIGHT, axis = 1)
     return MORNING, EVENING, NIGHT
 
-def find_duplicate(CARS, length, ws, accum_count, sheet, excel_name) -> int:
+def find_duplicate_cars(CARS, length, ws, accum_count, sheet, excel_name) -> int:
     """Función para encontrar los patrones repetidos según la longitud del patrón especificado.
     """
     hojas = ['N','S','E','O']
@@ -190,6 +208,31 @@ def find_duplicate(CARS, length, ws, accum_count, sheet, excel_name) -> int:
 
     return count
 
+def find_duplicate_pedestrian(MORNING, EVENING, NIGHT, length, ws, accum_count, excel_name) -> int:
+    count = accum_count
+
+    PEDESTRIAN = np.concatenate((MORNING, EVENING))
+    PEDESTRIAN = np.concatenate((PEDESTRIAN, NIGHT))
+
+    for k in range(PEDESTRIAN.shape[1]):
+        GIRO = PEDESTRIAN[:,k]
+        for i in range(len(GIRO)-length+1):
+            set = GIRO[i:i+length]
+            if np.array_equal(set,np.zeros(length)): #Delete zero cases
+                continue
+            for j in range(i+1, len(GIRO) - length + 1):
+                if np.array_equal(GIRO[j:j+length], set):
+                    if sum(set) <= 4: #No considering low duplicates
+                        continue
+                    result = ""
+                    for elem in set:
+                        result += str(elem) + ','
+                    ws.cell(row=count+2, column=1, value=result[:-2])
+                    ws.cell(row=count+2, column=2, value=excel_name)
+                    count += 1
+
+    return count
+
 def jump_single():
     pass
 
@@ -197,8 +240,7 @@ def jump_multiple():
     pass
 
 def main():
-    directory = r"C:\Users\dacan\OneDrive\Desktop\PRUEBAS\Supervisor\Entregable Nro 06\7.- Informacion de Campo\Vehicular\Raw_Data"
-
+    directory = r"C:\Users\dacan\OneDrive\Desktop\PRUEBAS\Supervisor\Entregable Nro 06\7.- Informacion de Campo\Peatonal\Tipico\Pruebitas"
     logger_path = os.path.join(directory,"LOGS")
     if not os.path.exists(logger_path):
         os.mkdir(logger_path)
@@ -207,33 +249,64 @@ def main():
     LOGGER.addHandler(fh)
     excels = os.listdir(directory)
     excels = [excel for excel in excels if excel.endswith('.xlsm') and '~$' not in excel]
-    new_excel = os.path.join(directory,"Pattern_Summary.xlsx")
-    if not os.path.exists(new_excel):
-        wb = Workbook()
-        wb.save(new_excel)
-        wb.close()
+    new_excel = os.path.join(directory, "Pattern_Summary.xlsx")
+    wb = Workbook()
+    wb.save(new_excel)
+    wb.close()
 
     wb = load_workbook(new_excel)
     ws = wb['Sheet']
 
-    #Encabezados:
     ws.cell(row=1, column=1, value="Patrón")
-    ws.cell(row=1, column=2, value="Hoja")
-    ws.cell(row=1, column=3, value="Excel")
+    ws.cell(row=1, column=2, value="Excel")
 
-    acumm_count = 0
-    for count, excel in enumerate(excels):
-        print(f"{count+1}/{len(excels)} Reading: {excel}")
-        CARS_LIST, MOTOS_LIST = read_excel_vehicular(os.path.join(directory, excel))
-        for sheet, CARS in enumerate(CARS_LIST): #CARS = Arreglo por sentido.
-            current_count = find_duplicate(CARS,4,ws,acumm_count, sheet,excel)
-            acumm_count = current_count
-        for sheet, MOTOS in enumerate(MOTOS_LIST):
-            current_count = find_duplicate(MOTOS,4,ws,acumm_count,sheet,excel)
-            acumm_count = current_count
+    accum_count = 0
+
+    for excel in excels:
+        MORNING, EVENING, NIGHT = read_excel_pedestrian(os.path.join(directory,excel))
+        current_count = find_duplicate_pedestrian(MORNING, EVENING, NIGHT, 4, ws, accum_count, excel)
+        accum_count = current_count
 
     wb.save(new_excel)
     wb.close()
+        
+    # directory = r"C:\Users\dacan\OneDrive\Desktop\PRUEBAS\Supervisor\Entregable Nro 06\7.- Informacion de Campo\Vehicular\Raw_Data"
+
+    # logger_path = os.path.join(directory,"LOGS")
+    # if not os.path.exists(logger_path):
+    #     os.mkdir(logger_path)
+    # fh = logging.FileHandler(os.path.join(directory,"LOGS","means.log"))
+    # fh.setFormatter(f)
+    # LOGGER.addHandler(fh)
+    # excels = os.listdir(directory)
+    # excels = [excel for excel in excels if excel.endswith('.xlsm') and '~$' not in excel]
+    # new_excel = os.path.join(directory,"Pattern_Summary.xlsx")
+    # if not os.path.exists(new_excel):
+    #     wb = Workbook()
+    #     wb.save(new_excel)
+    #     wb.close()
+
+    # wb = load_workbook(new_excel)
+    # ws = wb['Sheet']
+
+    # #Encabezados:
+    # ws.cell(row=1, column=1, value="Patrón")
+    # ws.cell(row=1, column=2, value="Hoja")
+    # ws.cell(row=1, column=3, value="Excel")
+
+    # acumm_count = 0
+    # for count, excel in enumerate(excels):
+    #     print(f"{count+1}/{len(excels)} Reading: {excel}")
+    #     CARS_LIST, MOTOS_LIST = read_excel_vehicular(os.path.join(directory, excel))
+    #     for sheet, CARS in enumerate(CARS_LIST): #CARS = Arreglo por sentido.
+    #         current_count = find_duplicate(CARS,4,ws,acumm_count, sheet,excel)
+    #         acumm_count = current_count
+    #     for sheet, MOTOS in enumerate(MOTOS_LIST):
+    #         current_count = find_duplicate(MOTOS,4,ws,acumm_count,sheet,excel)
+    #         acumm_count = current_count
+
+    # wb.save(new_excel)
+    # wb.close()
 
 if __name__ == '__main__':
     main()
